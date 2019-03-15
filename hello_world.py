6b6866f67b613b5e241020c4dfe009311adfe401
Steven Chan <schen1998@outlook.com>
2019-03-15 15:09:39 +0800

    add hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,22 @@
+print("Hello World")
+
+message = "Hello python WORLD"
+print(message)
+message = "Change Hello python wORLD"
+print(message)
+
+print(message.title())
+print(message.upper())
+print(message.lower())
+
+print("Student:\n\tSteven\n\tFrankie\n\tNiko" + '\nThey are "good" students!')
+
+message2 = "Python "
+print(message2)
+message2 = message2.rstrip()  # 这是一条注释：rstrip()用于去除空白
+print(message2)
+
+gpa = 5.0
+print("Amazing! you got a GPA: " + str(gpa))  # str()用于将其他类型转化为string类型，这样才能输出
+
+import this
