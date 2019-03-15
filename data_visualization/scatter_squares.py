69bf41f7e0cf03e2ce185db3357974a268a855c7
Steven Chan <schen1998@outlook.com>
2019-03-15 23:05:13 +0800

    multiple points

--- data_visualization/scatter_squares.py
+++ data_visualization/scatter_squares.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 
-plt.scatter(2, 4, s=200)
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
+plt.scatter(x_values, y_values)
 
 #设置图表标题并给坐标轴加上标签
 plt.title("Square Numbers", fontsize=24)
